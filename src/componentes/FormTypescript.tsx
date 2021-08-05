cb80a70a891f56233b960f00c5d21aa204a1aa4b
Piero Condor <piero.condor26@gmail.com>
2021-08-04 19:08:40 -0500

    crud, listado de items, funcion agregar y uso de interface

--- src/componentes/FormTypescript.tsx
+++ src/componentes/FormTypescript.tsx
@@ -1,21 +1,47 @@
 import React,{Fragment,useState} from 'react';
 
+type FormElement=React.FormEvent<HTMLFormElement>
+interface iTarea{
+    name:string,
+    done:boolean
+}
+
 const FormTypescript = () => {
 
     
-    const[tarea,setTarea]=useState('');  
+    const[tarea,setTarea]=useState<string>('');  //con ts establecemos el tipo de dato dentro de un estado
+    const[listaTareas,setListaTareas]=useState<iTarea[]>([]) //establecemos la interfaz iTarea para decir que listaTareas es un array de objetos
     
-    const enviar=()=>{
-        console.log('se envia')
+    const enviar=(e:FormElement)=>{ //el evento viene de react de un formulario html, tal como se indica en el type "formelement"
+        e.preventDefault()
+        agregarTarea(tarea)
+       setTarea('')
+    }
+
+    const agregarTarea=(name:string)=>{ //se esta guardando una tarea de tipo string, como se definio ya arriba
+        const nuevasTareas:iTarea[]=[...listaTareas,{name,done:false}]
+        setListaTareas(nuevasTareas)
     }
 
     return ( 
         <Fragment>
         <h2>Formulario</h2>
+        <div className="card p-3 mb-4">
         <form onSubmit={enviar}>
-            <input type="text" onChange={e=>setTarea(e.target.value)}/>
+            <input type="text" 
+            onChange={e=>setTarea(e.target.value)}
+            value={tarea} autoFocus/>
             <button>Guardar</button>
         </form>
+        </div>
+
+        <div className="card p-3">
+        {
+            listaTareas.map((ta:iTarea, index:number)=>{
+              return  <h3 key={index}>{ta.name} - {ta.done+''}</h3>
+            })
+        }
+        </div>
          </Fragment>
      );
 }
